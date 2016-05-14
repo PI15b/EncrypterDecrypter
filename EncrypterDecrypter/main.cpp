d5503870b7f9b96a2c09c7354dced955f6316d44
Dmitry <waild-cat@mail.ru>
2016-05-14 20:11:22 +0300

    Add files via upload
    Two buttons

--- EncrypterDecrypter/main.cpp
+++ EncrypterDecrypter/main.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QLabel>
+#include <QPushButton>
+#include <QHBoxLayout>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    QWidget *mainWindow = new QWidget;
+    mainWindow->setWindowTitle("Buttons");
+
+    QPushButton *encrypt = new QPushButton("Encrypt");
+     QPushButton *overview = new QPushButton("Overview");
+     QObject::connect(encrypt, SIGNAL(clicked()), , SLOT());
+      QObject::connect(overview, SIGNAL(clicked()), , SLOT());]
+
+      QHBoxLayout *buttons = new QHBoxLayout;
+      buttons->addWidget(encrypt);
+      buttons->addWidget(overview);
+
+    mainWindow->setLayout(buttons);
+    mainWindow->show();
 
     return a.exec();
 }
